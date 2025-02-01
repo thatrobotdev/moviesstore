adce9104f9203baeed819588e370416a8d1490d5
Palash Patel <palashpatel@No-Laptop-Pro.lan>
2025-02-01 01:13:37 -0500

    chap 12; done. (this is chap 13's step)

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 @login_required
 def logout(request):
     auth_logout(request)
@@ -43,3 +45,11 @@ def signup(request):
         template_data['form'] = form
         return render(request, 'accounts/signup.html',
  {'template_data': template_data})
+    
+@login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html',
+        {'template_data': template_data})
