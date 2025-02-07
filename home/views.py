393b4e2cd40b60c5d02a1f03c774e56298598390
Aryamann <aryamannsheoran@gmail.com>
2025-02-07 16:04:25 -0500

    addded three random movies to the front page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
+import random
 from django.shortcuts import render
+from movies.models import Movie
 
 def index(request):
+    movies = list(Movie.objects.all()) 
+    random_movies = random.sample(movies, min(3, len(movies)))
+
     template_data = {}
     template_data['title'] = 'Movies Store'
+    template_data['random_movies'] = random_movies
     return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
     template_data = {}
-    template_data['title'] = 'About'
+    template_data = {'title': 'About'}
     return render(request, 'home/about.html', {'template_data': template_data})
